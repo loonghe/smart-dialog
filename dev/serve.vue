22a62a1e0bd785d26fa0dee3934d09bf6c3c18b9
looongda <dragonhe7458@gmail.com>
2024-02-22 13:32:44 +0800

    feat: 利用vue-sfc-rollup脚手架初始化的自研vue组件npm发布的示例工程,踩坑解决打包编译问题，并引入scss

---
+++ dev/serve.vue
@@ -0,0 +1,17 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+import SmartDialog from '@/smart-dialog.vue';
+
+export default defineComponent({
+  name: 'ServeDev',
+  components: {
+    SmartDialog
+  }
+});
+</script>
+
+<template>
+  <div id="app">
+    <smart-dialog />
+  </div>
+</template>
