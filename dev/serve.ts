98e319417cbcf7c876149b805f1670498c5de0f6
loonghe <loooonghe@gmail.com>
2024-03-02 08:43:28 +0800

    工程中引入element-plus组件，提高开发自定义业务组件效率

--- dev/serve.ts
+++ dev/serve.ts
@@ -1,5 +1,8 @@
-import { createApp } from 'vue';
-import Dev from './serve.vue';
+import { createApp } from "vue";
+import { ElInput } from "element-plus";
+import Dev from "./serve.vue";
+import "element-plus/dist/index.css";
 
 const app = createApp(Dev);
-app.mount('#app');
+app.use(ElInput);
+app.mount("#app");
--- src/smart-dialog.vue
+++ src/smart-dialog.vue
@@ -1,5 +1,5 @@
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent } from "vue";
 
 interface SampleData {
   counter: number;
@@ -8,10 +8,11 @@ interface SampleData {
     action: string | null;
     amount: number | null;
   };
+  customInput: string;
 }
 
-export default /*#__PURE__*/defineComponent({
-  name: 'SmartDialog', // vue component name
+export default /*#__PURE__*/ defineComponent({
+  name: "SmartDialog", // vue component name
   data(): SampleData {
     return {
       counter: 5,
@@ -20,31 +21,32 @@ export default /*#__PURE__*/defineComponent({
         action: null,
         amount: null,
       },
+      customInput: "this is element-plus component",
     };
   },
   computed: {
     changedBy() {
       const { message } = this as SampleData;
-      if (!message.action) return 'initialized';
-      return `${message.action} ${message.amount || ''}`.trim();
+      if (!message.action) return "initialized";
+      return `${message.action} ${message.amount || ""}`.trim();
     },
   },
   methods: {
     increment(arg: Event | number): void {
-      const amount = (typeof arg !== 'number') ? 1 : arg;
+      const amount = typeof arg !== "number" ? 1 : arg;
       this.counter += amount;
-      this.message.action = 'incremented by';
+      this.message.action = "incremented by";
       this.message.amount = amount;
     },
     decrement(arg: Event | number): void {
-      const amount = (typeof arg !== 'number') ? 1 : arg;
+      const amount = typeof arg !== "number" ? 1 : arg;
       this.counter -= amount;
-      this.message.action = 'decremented by';
+      this.message.action = "decremented by";
       this.message.amount = amount;
     },
     reset(): void {
       this.counter = this.initCounter;
-      this.message.action = 'reset';
+      this.message.action = "reset";
       this.message.amount = null;
     },
   },
@@ -53,38 +55,35 @@ export default /*#__PURE__*/defineComponent({
 
 <template>
   <div class="smart-dialog">
-    <p>The counter was {{ changedBy }} to <b>{{ counter }}</b>.</p>
-    <button @click="increment">
-      Click +1
-    </button>
-    <button @click="decrement">
-      Click -1
-    </button>
-    <button @click="increment(5)">
-      Click +5
-    </button>
-    <button @click="decrement(5)">
-      Click -5
-    </button>
-    <button @click="reset">
-      Reset
-    </button>
+    <p>
+      The counter was {{ changedBy }} to <b>{{ counter }}</b
+      >.
+    </p>
+    <button @click="increment">Click +1</button>
+    <button @click="decrement">Click -1</button>
+    <button @click="increment(5)">Click +5</button>
+    <button @click="decrement(5)">Click -5</button>
+    <button @click="reset">Reset</button>
+    <!-- 基于 element-plus 组件进行二次开发, <p></p>表示空行 -->
+    <p></p>
+    <hr />
+    <el-input v-model="customInput" clearable placeholder="请输入..." />
   </div>
 </template>
 
 <style scoped lang="scss">
-  .smart-dialog {
-    display: block;
-    width: 400px;
-    margin: 25px auto;
-    border: 1px solid #ccc;
-    background: #eaeaea;
-    text-align: center;
-    padding: 25px;
+.smart-dialog {
+  display: block;
+  width: 400px;
+  margin: 25px auto;
+  border: 1px solid #ccc;
+  background: #eaeaea;
+  text-align: center;
+  padding: 25px;
+}
+.smart-dialog {
+  p {
+    margin: 0 0 1em;
   }
-  .smart-dialog {
-    p {
-      margin: 0 0 1em;
-    }
-  }
+}
 </style>
